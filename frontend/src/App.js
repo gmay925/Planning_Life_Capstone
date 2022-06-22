5e308917836cf8f822e783bb1b68554130a4e1b5
Gia May <gia.may@wwt.com>
2022-06-22 14:26:05 -0400

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import LandingPage from './Components/LandingPage';
+import Login from './Components/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route exact path='/' element={<LandingPage />} />
+      <Route exact path='/login' element={<Login />} />
+    </Routes>
+    
 }
 
 export default App;
