588015e275463bd4370facccee4d604dbf00f78e
Gia May <gia.may@wwt.com>
2022-07-30 18:28:07 -0400

    update

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -1,29 +1,29 @@
-// const jwt = require('jsonwebtoken');
-// const asyncHandler = require('express-async-handler');
-// const User = require('../models/user');
-// const createError = require('http-errors')
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
+const createError = require('http-errors')
 
-// const protect = asyncHandler(async (req, res, next) => {
-//   const { token } = req.cookies;
+const protect = asyncHandler(async (req, res, next) => {
+  const { token } = req.cookies;
 
-//   if (!token) {
-//     res.status(401);
-//     // throw new Error('Not authorized - no token.');
-//     return next(createError.Unauthorized('Not authorized - no token.'));
-//   }
+  if (!token) {
+    res.status(401);
+    // throw new Error('Not authorized - no token.');
+    return next(createError.Unauthorized('Not authorized - no token.'));
+  }
 
-//   try {
-//     // Verify token
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-//     // Get user from token
-//     req.user = await User.findById(decoded.id).select('-hashPassword');
+    // Get user from token
+    req.user = await User.findById(decoded.id).select('-hashPassword');
 
-//     next();
-//   } catch (error) {
-//     res.status(401);
-//     throw new Error('Not authorized.');
-//   }
-// });
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error('Not authorized.');
+  }
+});
 
-// module.exports = { protect };
+module.exports = { protect };
